f16aa32cec6d5cc92acf3b8e4e7146bc4016ff88
STS <steffsch@student.ethz.ch>
2020-05-23 23:34:33 -0400

    Fixed Flask bug

--- SMS_Twilio_backend.py
+++ SMS_Twilio_backend.py
@@ -6,6 +6,7 @@ import system_constants
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 
+app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
