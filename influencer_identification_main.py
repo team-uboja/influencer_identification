b7c258edb9eedbfbe8e185a8654049e04de1fd0e
STS <steffsch@student.ethz.ch>
2020-06-01 18:52:12 -0400

    abc

--- influencer_identification_main.py
+++ influencer_identification_main.py
@@ -107,7 +107,7 @@ def login():
 
 #login handling
 
-@login.user_loader
+@flask_login.user_loader
 def load_user(user_id):
     user_data = utils.utils.readFromUserDB(user_id)
     user = User.User(user_data['username'], user_data['mail'])
