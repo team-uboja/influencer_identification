245c4102491ff77e034a3f24168dd8773221c92b
STS <steffsch@student.ethz.ch>
2020-06-01 20:12:58 -0400

    abc

--- influencer_identification_main.py
+++ influencer_identification_main.py
@@ -26,7 +26,7 @@ app = Flask(__name__, static_folder= './static')
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 login_manager.session_protection = "strong"
-#login_manager.login_view = '/login'
+login_manager.login_view = '/login'
 
 
 app.config['UPLOAD_FOLDER'] = system_constants.UPLOAD_FOLDER
@@ -86,24 +86,24 @@ def login():
     # Here we use a class of some kind to represent and validate our
     # client-side form data. For example, WTForms is a library that will
     # handle this for us, and we use a custom LoginForm to validate.
-    form = wtforms.LoginForm()
-    if form.validate_on_submit():
+    #form = wtforms.LoginForm()
+    #if form.validate_on_submit():
         # Login and validate the user.
         # user should be an instance of your `User` class
-        if utils.utils.check_password(form.username.data, form.password.data):
+    #    if utils.utils.check_password(form.username.data, form.password.data):
 
-            login_manager.login_user(User.User(form.username.data))
+#            login_manager.login_user(User.User(form.username.data))
 
-            flash('Logged in successfully.')
+#            flash('Logged in successfully.')
 
-            next = request.args.get('next')
+ #           next = request.args.get('next')
             # is_safe_url should check if the url is safe for redirects.
             # See http://flask.pocoo.org/snippets/62/ for an example.
-            if not flask.is_safe_url(next):
-                return flask.abort(400)
+  #          if not flask.is_safe_url(next):
+   #             return flask.abort(400)
 
-            return flask.redirect(next or flask.url_for('home'))
-    return render_template('login.html', form=form)
+    #        return flask.redirect(next or flask.url_for('home'))
+    return render_template('login.html')
 
 
 #login handling
